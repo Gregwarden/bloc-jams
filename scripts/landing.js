38ffe79a84d4339611d86436a2d93055fc7b1078
Gregwarden <grewarden@gmail.com>
2017-06-30 09:28:39 -0700

    Checkpoint 8 Completed, Animate not working

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,31 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-     var points = document.getElementsByClassName('point');
-     var revealPoints = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
+var revealPoints = function(point) {
+   point.style.opacity = 1;
+   point.style.transform = "scaleX(1) translateY(0)";
+   points.style.msTransform = "scaleX(1) translateY(0)";
+   points.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
+var animatePoints = function(points) {
+     forEach(points, revealPoints);
+
      for (var b = 0; b < points.length; b++) {
        revealPoints(b);
      }
+
+     window.onload = function() {
+       if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+       var sellingPoints = document.getElementsByClassName('selling-points')[0]
+       var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+       window.addEventListener('scroll', function(event) {
+          console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+         }
+   });
+ }
  };
---
+++ scripts/utilities.js
@@ -0,0 +1,5 @@
+function forEach(array, callback) {
+   for (var i= 0; i < arr.length; i++) {
+       callback(arr[i]);
+   }
+}
